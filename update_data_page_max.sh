e63fa29a6517addb49f6610775ab855bb66674e5
Maximilian Leo Huber <huber.maximilian.leo@gmail.com>
2023-12-07 12:53:39 +0100

    reintroducing raw data

--- update_data_page_max.sh
+++ update_data_page_max.sh
@@ -49,6 +49,12 @@ plot '< tail -n $N_data /home/pi/data-logger/data/ads1115.txt' using 1:3" | gnup
   echo '<h1>Temperature (Sensor: ads1115)</h1>' >> $page
   echo '<img src="ads1115_1.png">' >> $page
   echo '<img src="ads1115_2.png">' >> $page
+  echo '<br><details>' >> $page
+  echo '<summary>raw data</summary>' >> $page
+  echo '<pre>' >> $page
+  tail -n $N_data /home/pi/data-logger/data/ads1115.txt >> $page
+  echo '</pre>' >> $page
+  echo '</details>' >> $page
   echo '</section>' >> $page
         
   echo "set terminal png size 600,400
@@ -71,6 +77,12 @@ plot '< tail -n $N_data /home/pi/data-logger/data/bme280.txt' using 1:3" | gnupl
   echo '          <h1>Pressure (Sensor: bme280)</h1> ' >> $page
   echo '          <img src="bme280_1.png"> ' >> $page
   echo '    <img src="bme280_2.png"> ' >> $page
+  echo '<br><details>' >> $page
+  echo '<summary>raw data</summary>' >> $page
+  echo '<pre>' >> $page
+  tail -n $N_data /home/pi/data-logger/data/bme280.txt >> $page
+  echo '</pre>' >> $page
+  echo '</details>' >> $page
   echo '</section>' >> $page       
         
   echo "set terminal png size 600,400
@@ -93,6 +105,12 @@ plot '< tail -n $N_data /home/pi/data-logger/data/ds18b20.txt' using 1:3" | gnup
   echo '    <h1>Volt (Sensor: ds18b20)</h1>' >> $page 
   echo '    <img src="ds18b20_1.png">' >> $page 
   echo '    <img src="ds18b20_2.png">' >> $page 
+  echo '<br><details>' >> $page
+  echo '<summary>raw data</summary>' >> $page
+  echo '<pre>' >> $page
+  tail -n $N_data /home/pi/data-logger/data/ds18b20.txt >> $page
+  echo '</pre>' >> $page
+  echo '</details>' >> $page
   echo '</section>' >> $page
   
   echo "set terminal png size 600,400
@@ -106,6 +124,12 @@ plot '< tail -n $N_data /home/pi/data-logger/data/xgzp6847d.txt' using 1:2" | gn
   echo '<section id="Ampere">' >> $page
   echo '    <h1>Ampere (Sensor: xgzp6847d)</h1> ' >> $page
   echo '    <img src="xgzp6847d_1.png"> ' >> $page
+  echo '<details>' >> $page
+  echo '<summary>raw data</summary>' >> $page
+  echo '<pre>' >> $page
+  tail -n $N_data /home/pi/data-logger/data/xgzp6847d.txt >> $page
+  echo '</pre>' >> $page
+  echo '</details>' >> $page
   echo '</section>' >> $page
   
   echo "set terminal png size 600,400
@@ -128,6 +152,12 @@ plot '< tail -n $N_data /home/pi/data-logger/data/mlx90614.txt' using 1:3" | gnu
   echo '    <h1>Water Temperature (Sensor: mlx90614)</h1>' >> $page
   echo '    <img src="mlx90614_1.png"> ' >> $page
   echo '    <img src="mlx90614_2.png">' >> $page
+  echo '<br><details>' >> $page
+  echo '<summary>raw data</summary>' >> $page
+  echo '<pre>' >> $page
+  tail -n $N_data /home/pi/data-logger/data/mlx90614.txt >> $page
+  echo '</pre>' >> $page
+  echo '</details>' >> $page
   echo '</section>     ' >> $page
                
   echo "set terminal png size 600,400
@@ -141,6 +171,12 @@ plot '< tail -n $N_data /home/pi/data-logger/data/max31865.txt' using 1:2" | gnu
   echo ' <section id="Infrared">' >> $page
   echo '    <h1>Infrared Temperature (Sensor: max31865)</h1> ' >> $page
   echo '    <img src="max31865_1.png">' >> $page
+  echo '<br><details>' >> $page
+  echo '<summary>raw data</summary>' >> $page
+  echo '<pre>' >> $page
+  tail -n $N_data /home/pi/data-logger/data/max31865.txt >> $page
+  echo '</pre>' >> $page
+  echo '</details>' >> $page
   echo '</section>' >> $page
   
   echo "set terminal png size 600,400
@@ -191,6 +227,12 @@ plot '< tail -n $N_data /home/pi/data-logger/data/bronkhorst_propar.txt' using 1
   echo '    <img src="bronkhorst_propar_3.png"> ' >> $page
   echo '    <img src="bronkhorst_propar_4.png"> ' >> $page
   echo '    <img src="bronkhorst_propar_5.png"> ' >> $page
+  echo '<br><details>' >> $page
+  echo '<summary>raw data</summary>' >> $page
+  echo '<pre>' >> $page
+  tail -n $N_data /home/pi/data-logger/data/bronkhorst_propar.txt >> $page
+  echo '</pre>' >> $page
+  echo '</details>' >> $page
   echo '</section>     ' >> $page
   echo '</div>' >> $page
     
